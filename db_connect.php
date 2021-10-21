33be9013d0a1e7459402067a1b07553a2440ad32
posilya <pos_ilya@icloud.com>
2021-10-21 14:53:33 +0500

    Отрисовка таблицы на стороне сервера

--- db_connect.php
+++ db_connect.php
@@ -2,8 +2,8 @@
 require_once 'pdo_config.php';
 
 try {
-    $connection = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+	$db_conn = new PDO("mysql:host=$host;dbname=$db", $user, $password);
 } catch (PDOException $pe) {
-    die("Ничего не вышло: " . $pe->getMessage());
+	die("Ничего не вышло: " . $pe->getMessage());
 }
 ?>
--- public_html/index.php
+++ public_html/index.php
@@ -1,7 +1,6 @@
 <?php
 require_once(realpath('../db_connect.php'));
 ?>
-
 <!DOCTYPE html>
 <html lang="ru">
 	<head>
@@ -14,226 +13,37 @@ require_once(realpath('../db_connect.php'));
 			<h1>Телефонный справочник</h1>
 		</header>
 		<main>
-			<table>
+			<?php
+			$query = $db_conn->prepare("SELECT * FROM `main` ORDER BY `id`");
+			$query->execute();
+
+			$contacts = $query->fetchAll(PDO::FETCH_ASSOC);
+
+			if (empty($contacts)) {
+				echo '<p>Мне нечего тебе показать :(</p>';
+			} else {
+				?>
+				<table>
 				<tr>
 					<th>ФИО</th>
 					<th>Телефон</th>
 					<th>Кем приходится</th>
 					<th>Действие</th>
 				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-				<tr db="1">
-					<td>Тест&nbsp;Тестов Тестович</td>
-					<td><a href="tel:+79537102649">+7&nbsp;953&nbsp;710&nbsp;26&nbsp;49</a></td>
-					<td>Банк</td>
-					<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>
-				</tr>
-			</table>
+				<?php
+				foreach ($contacts as $contact) {
+					echo '<tr db="' . $contact[id] . '">';
+					echo '<td>' . $contact[name] . '</td>';
+					echo '<td><a href="tel:' . $contact[phone] . '">' . $contact[phone] . '</a></td>';
+					echo '<td>' . $contact[note] . '</td>';
+					echo '<td><a class="delete">Удалить</a> / <a class="edit">Редактировать</a></td>';
+					echo '</tr>';
+				}
+				echo '</table>';
+			}
+			?>
 			<div><a class="add">Добавить</a></div>
 		</main>
-		
-		<!-- добавить контакт
-		<div class="popup-bg">
-			<div class="popup">
-				<h1>Добавить контакт</h1>
-
-				<div>
-					<div>
-						<label for="l_name">Фамилия</label>
-						<input type="text" id="l_name">
-					</div>
-
-					<div>
-						<label for="name">Имя</label>
-						<input type="text" id="name">
-					</div>
-
-					<div>
-						<label for="patronymic">Отчество</label>
-						<input type="text" id="patronymic">
-					</div>
-				</div>
-
-				<div>
-					<label for="phone">Телефон</label>
-					<input type="tel"id="phone">
-				</div>
-
-				<div>
-					<label for="note">Кем приходится</label>
-					<input type="text" id="note">
-				</div>
-
-				<a class="save">Сохранить</a><a class="cancel">Отмена</a>
-			</div>
-		</div>
-		-->
-
-		<!-- поп-ап обновить
-		<div class="popup-bg">
-			<div class="popup">
-				<h1>Изменить контакт</h1>
-
-				<div>
-					<div>
-						<label for="l_name">Фамилия</label>
-						<input type="text" id="l_name">
-					</div>
-
-					<div>
-						<label for="name">Имя</label>
-						<input type="text" id="name">
-					</div>
-
-					<div>
-						<label for="patronymic">Отчество</label>
-						<input type="text" id="patronymic">
-					</div>
-				</div>
-
-				<div>
-					<label for="phone">Телефон</label>
-					<input type="tel"id="phone">
-				</div>
-
-				<div>
-					<label for="note">Кем приходится</label>
-					<input type="text" id="note">
-				</div>
-
-				<a class="save">Обновить</a><a class="cancel">Отмена</a>
-			</div>
-		</div>
-		-->
-
-		<!-- поп-ап удалить
-		<div class="popup-bg">
-			<div class="popup">
-				<h1>Удалить контакт?</h1>
-
-				<a class="delete">Удалить</a><a class="drop">Отмена</a>
-			</div>
-		</div>
-		-->
 
 		<script src="script.js"></script>
 	</body>
